fb325ecd01506b7d73820ea1e113ef71febad81e
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 06:32:06 +0000

    7lab6

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 
-inputvalues = input('Enter all elements values: ')
-numbers1 = inputvalues.split() 
-for i in range(len(numbers1)):
-	numbers1[i] = int(numbers1[i]) 
 # The following line is the same as the for-loop
 # numbers1 = list(map(int, numbers))
 
@@ -11,3 +7,12 @@ for i in range(len(numbers1)):
 # ******************************
 # Make your Code
 # ******************************
+
+inputvalues = input('Enter all elements values: ')
+numbers1 = inputvalues.split() 
+numbers2 = []
+for i in range(len(numbers1)):
+	numbers1[i] = int(numbers1[i]) 
+for j in range(len(numbers1)):
+    numbers2.append(numbers1.pop())
+print(numbers2)
